feeb075308931b06edf411ec67d5a167f077b2eb
John King <61046562+JohnH-King@users.noreply.github.com>
2022-11-07 19:18:37 +0200

    Delete SystemT directory

--- SystemT/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SystemT.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
