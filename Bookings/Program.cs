5ea7773b2aa1eef0acf0afa008b516d70816ee67
JancoWeyers2601 <jancoweyers@gmail.com>
2022-11-07 20:15:14 +0200

    Add CRUD functionality
    New Controllers and views added

--- Bookings/Program.cs
+++ Bookings/Program.cs
@@ -1,7 +1,11 @@
+using Bookings.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SystemTContext>(options => options.UseSqlServer("Server=systemtise.csp1vikprmva.us-east-2.rds.amazonaws.com;Database=SystemT;User Id=admin;Password=SystemTTravel;Trusted_Connection=False"));
 
 var app = builder.Build();
 
