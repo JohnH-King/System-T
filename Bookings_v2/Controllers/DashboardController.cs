6009626f953c86c5126509bda0110e8ed6bad0ac
John King <Johnk858@ymail.com>
2022-11-11 07:33:28 +0200

    Dashboard on Home
    !NOT LANDING there after login

---
+++ Bookings_v2/Controllers/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿using Bookings_v2.Data;
+using Bookings_v2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookings_v2.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly SystemTContext _context;
+
+        public DashboardController(SystemTContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Flight> flights = await _context.Flights.Where(s => s.UserId.Contains(User.Identity.Name.ToString())).ToListAsync();
+            List<Transaction> transactions = await _context.Transactions.ToListAsync();           
+
+            ViewBag.Flights = flights;
+            ViewBag.Transactions = transactions;
+            return View();
+        }
+
+        
+    }
+}
